af7188292621e12746f7fba873c6d5aa47077ce4
Tom Aalbers <tom@dtaalbers.com>
2024-02-07 18:42:08 +0100

    Don’t redirect when on redirect page to prevent loop

--- src/infra/hook.ts
+++ src/infra/hook.ts
@@ -6,6 +6,7 @@ export class TheHook {
     public async canLoad(component: any, params: Parameters, instruction: RoutingInstruction, nav: Navigation) {
         console.log('TheHook canLoad');
         return true;
+        // if (instruction.component.name === 'redirect-page') return true;
         // return '/redirect';
     }
 }
